0772b742790cb680d744c57aeebcbb76f345a9c3
CharlesKibet <charlesbiegon973@gmail.com>
2024-05-26 13:03:22 +0300

    Lab complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Dog:
+    
+    def __init__(self,name,breed = "Mutt"):
+      self.name = name
+      self.breed = breed
+     
+    def bark(self):
+        print("Woof!")
+         
+    def showing_self(self):
+        return self
+    
+    def get_adopted(self,owner_name):
+        self.owner = owner_name
     pass
+
+fido = Dog("Fido")
+fido.get_adopted("Sophie")
+print(fido.owner)
+
+snoopy = Dog("Snoopy")
+snoopy.name
